077658d9ce8b80b875d8f08fe7d964b049c10bd0
Ismael <ismaelrodriguezforonda@gmail.com>
2025-02-11 17:05:49 +0100

    Starting to save variables

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ window.title("Continuous Kombucha Brewing Calculator")
 
 tabControl = ttk.Notebook(window)
 calculator = ttk.Frame(tabControl)
-Constants = ttk.Frame(tabControl)
+constants = ttk.Frame(tabControl)
 
 tabControl.add(calculator, text='Calculator')
-tabControl.add(Constants, text='Constants')
+tabControl.add(constants, text='Constants')
 
 tabControl.pack(expand=1, fill="both")
-
-ttk.Label(calculator, text="Welcome to GeeksForGeeks").grid(column=0, row=0, padx=30, pady=30)
-ttk.Label(Constants, text="Lets gut going with some fermentation").grid(column=0, row=0, padx=30, pady=30)
 
 def update_brew(*args):
     try:
@@ -154,5 +151,28 @@ num_theoretical_brew.trace("w", update_brew) # "w" means write (changes to the v
 #Test Button
 button = tkinter.Button(calculator, text="Submit", command="")
 button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
+
+#Constants
+
+brewVesselConstant_frame = tkinter.LabelFrame(constants, text="Brewing Vessel Info")
+brewVesselConstant_frame.grid(row=0, column=0)
+
+brewVessel_name = tkinter.Label(brewVesselConstant_frame, text="Vessel Name")
+brewVessel_name.grid(row=0, column=0)
+brewVessel_unit = tkinter.Label(brewVesselConstant_frame, text="Vessel Weight (g)")
+brewVessel_unit.grid(row=0, column=1)
+
+testbrewconstant1 = tkinter.StringVar(value="502")
+testbrewconstant2 = tkinter.StringVar(value="504")
+
+brewVesselConstant_v1_label = tkinter.Label(brewVesselConstant_frame, text="Vessel 1")
+brewVesselConstant_v1_label.grid(row=1, column=0)
+brewVesselConstant_v1_value = tkinter.Label(brewVesselConstant_frame, textvariable=testbrewconstant1)
+brewVesselConstant_v1_value.grid(row=1, column=1)
+
+brewVesselConstant_v2_label = tkinter.Label(brewVesselConstant_frame, text="Vessel 2")
+brewVesselConstant_v2_label.grid(row=2, column=0)
+brewVesselConstant_v2_value = tkinter.Label(brewVesselConstant_frame, textvariable=testbrewconstant2)
+brewVesselConstant_v2_value.grid(row=2, column=1)
 
 window.mainloop()
